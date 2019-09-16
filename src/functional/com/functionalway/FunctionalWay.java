01dd39a35cdc6e2ea2a11342883a00ec86924ba3
prakashelango <prakashcse100@gmail.com>
2019-09-16 16:39:07 +0530

    Functional Programming.

---
+++ src/functional/com/functionalway/FunctionalWay.java
@@ -0,0 +1,34 @@
+package functional.com.functionalway;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+public class FunctionalWay {
+
+    public static void main(String[] args) {
+        FunctionalWay fw = new FunctionalWay();
+        System.out.println(fw.legacyifElse("Hello"));
+        System.out.println(fw.legacyifElse(null));
+
+        System.out.println(fw.newIfElse().apply("Hello"));
+        System.out.println(fw.newIfElse().apply(null));
+    }
+
+    /*Return Else Condition if input is null or Convert to lower*/
+    private String legacyifElse(String s) {
+        if (s != null) {
+            return s.toLowerCase();
+        } else {
+            return "Value is null So else in Legacy.";
+        }
+    }
+
+    /*Return Else Condition if input is null or Convert to lower*/
+    private Function<String, String> newIfElse() {
+        return s -> Optional.ofNullable(s)
+                .map(y -> y.toLowerCase())
+                .orElse("Value is null So else in Functional.");
+    }
+
+
+}
